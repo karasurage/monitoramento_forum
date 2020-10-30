b7c819b3e770717215b7c1f9df3350023b0822a4
karasurage <nicholas.mateus@gmail.com>
2020-10-30 17:41:52 -0300

    first commit

---
+++ src/test/java/br/com/forum/monitoramento/MonitoramentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.forum.monitoramento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoramentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
